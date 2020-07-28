e35a16d9411af6b75d81ca2ba8c3b7ecae2d6435
HelipengTony <tony.hlp@hotmail.com>
2020-07-29 01:51:40 +0800

    update ui

--- index.tsx
+++ index.tsx
@@ -1,18 +1,18 @@
-import 'react-app-polyfill/ie11';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { NexmentContainer } from 'nexment';
+import "react-app-polyfill/ie11";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { NexmentContainer } from "nexment";
 
 const App = () => {
   const config = {
     leancloud: {
-      appId: '6Kcb9HB1iOR87HUbAvnMyUER-gzGzoHsz',
-      appKey: 'zf3gAf9CGIHLL8Crctb0sJLV',
-      serverURL: 'https://leancloud.ouorz.com',
+      appId: "6Kcb9HB1iOR87HUbAvnMyUER-gzGzoHsz",
+      appKey: "zf3gAf9CGIHLL8Crctb0sJLV",
+      serverURL: "https://leancloud.ouorz.com",
     },
     admin: {
-      name: 'TonyHe',
-      email: 'he@holptech.com',
+      name: "TonyHe",
+      email: "he@holptech.com",
     },
   };
   return (
@@ -20,16 +20,20 @@ const App = () => {
       <NexmentContainer config={config} />
       <style>
         {`
+        body{
+          margin:0px;
+        }
         .example-container{
           width:650px;
           margin:0 auto;
           padding: 20px;
           border-left: 1px solid #eee;
           border-right: 1px solid #eee;
+          min-height: 100vh;
         }`}
       </style>
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
