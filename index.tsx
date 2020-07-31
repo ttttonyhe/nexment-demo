712727a29ce168faadf451de3c36e3167c2530ae
HelipengTony <tony.hlp@hotmail.com>
2020-07-31 16:04:09 +0800

    update config

--- index.tsx
+++ index.tsx
@@ -6,6 +6,7 @@ import { NexmentContainer } from "nexment";
 const App = () => {
   const config = {
     pageKey: 'demoPage',
+    enableLinkInput:true,
     leancloud: {
       appId: "6Kcb9HB1iOR87HUbAvnMyUER-gzGzoHsz",
       appKey: "zf3gAf9CGIHLL8Crctb0sJLV",
