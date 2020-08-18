349ea5ecf427cc854cd1c8e0e3e1b91b1aa01a1a
HelipengTony <tony.hlp@hotmail.com>
2020-08-18 14:07:09 +0800

    update something

--- index.tsx
+++ index.tsx
@@ -25,7 +25,7 @@ const App = () => {
           <h1>Nexment</h1>
         </div>
         <div>
-          <a href="https://nexment-demo.ouorz.com" target="_blank">
+          <a href="https://nexment-vue-demo.ouorz.com" target="_blank">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -40,7 +40,7 @@ const App = () => {
               />
             </svg>
           </a>
-          <a href="https://github.com/HelipengTony/nexment-vue" target="_blank">
+          <a href="https://github.com/HelipengTony/nexment" target="_blank">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
